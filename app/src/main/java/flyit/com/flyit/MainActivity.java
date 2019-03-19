72f14d75f380c47e93e88e25a0a02a0a92bcb21f
Ibrahim Darakcilar <idarakcilar@gmail.com>
2019-03-19 23:18:56 +0300

    Initial commit

---
+++ app/src/main/java/flyit/com/flyit/MainActivity.java
@@ -0,0 +1,13 @@
+package flyit.com.flyit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
